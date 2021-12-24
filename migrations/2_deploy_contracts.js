ccb58e62daef909ad0e2509a39ebe81a4d334925
Ethan Cemer <tylercemer@gmail.com>
2021-12-24 10:41:15 -0600

    Added migration script, configured truffle config file to work with ganache and restructured file tree

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,4 @@
+const TodoList = artifacts.require("TodoList");
+module.exports = function (deployer) {
+  deployer.deploy(TodoList)
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
